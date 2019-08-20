efc03205b9019336012bb8ee08561427af2adefa
Caio Martins <caiofernandes14@gmail.com>
2019-08-20 12:44:37 -0300

    Codigo que verifica se existe arquivos .xlsx criados por mais de 30 min.

--- teste.sh
+++ teste.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
+###Codigo que verifica se existe arquivos .xlsx criados por mais de 30 min
 
-
+# Array dos diretorios a ser verificados
 diretorios=(
-    /home/caiomartins/Documentos/shell/opt/01  
-    /home/caiomartins/Documentos/shell/opt/02   
+    Diretorio/01 #colocar espaço 
+    diretorios/02 #sem espaço. 
 )
 
+
+# Verifica cada diretorio do array
 for ((i=0; i<${#diretorios[@]}; i++))
 do
-    f=$(find ${diretorios[$i]} -name "*.xlsx" -mmin +30 | wc -l)
+    # Pesquisa os aquivos no diretorios com mais de 30 min e retorna a quantidade
+    find=$(find ${diretorios[$i]} -name "*.xlsx" -mmin +30 | wc -l)
     
-    if [[ $f -ge 0 ]];then
-        arr+="${diretorios[$i]}"            
+    # Verifica se $find é maior que 0 e add diretorio ao array
+    if [[ $find -ge 0 ]];then
+        arr_diretorios+="${diretorios[$i]}"            
     fi
-    
+    # Soma a quantidade de arquivos de cada diretorio
     total=$[$total+$f]
 done
 
-echo $total
-echo ": "${arr[@]}
+echo "Total de arquivos: "$total
+echo "Diretorios que contem os arquivos: "${arr_diretorios[@]}
